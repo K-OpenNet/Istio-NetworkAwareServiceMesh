fb08522fa816d1dab2dc241566fb154138043c64
Chrome K <cr2025x1@gmail.com>
2018-03-05 20:43:56 +0900

    Gets JSON request as text

--- src/gowiki.go
+++ src/gowiki.go
@@ -1,95 +1,35 @@
 package main
 
 import (
-    "log"
-  	"io/ioutil"
-    "net/http"
-    "html/template"
-    "regexp"
-    "errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
-type Page struct {
-    Title string
-    Body  []byte
-}
-
 var err error
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+type User struct {
+	Id      string
+	Balance uint64
+}
 
 func main() {
-    http.HandleFunc("/view/", makeHandler(viewHandler))
-    http.HandleFunc("/edit/", makeHandler(editHandler))
-    http.HandleFunc("/save/", makeHandler(saveHandler))
-    log.Fatal(http.ListenAndServe(":8080", nil))
-}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// var u User
+		if r.Body == nil {
+			http.Error(w, "Please send a request body", 400)
+			return
+		}
 
-func (p *Page) save() error {
-    filename := p.Title + ".txt"
-    return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-    filename := title + ".txt"
-    body, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return nil, err
-    }
-    return &Page{Title: title, Body: body}, nil
-}
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        m := validPath.FindStringSubmatch(r.URL.Path)
-        if m == nil {
-            http.NotFound(w, r)
-            return
-        }
-        fn(w, r, m[2])
-    }
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-    p, err := loadPage(title)
-    if err != nil {
-        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-        return
-    }
-    renderTemplate(w, "view", p)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-    p, err := loadPage(title)
-    if err != nil {
-        p = &Page{Title: title}
-    }
-    renderTemplate(w, "edit", p)
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-    err := templates.ExecuteTemplate(w, tmpl+".html", p)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-    body := r.FormValue("body")
-    p := &Page{Title: title, Body: []byte(body)}
-    err = p.save()
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-    m := validPath.FindStringSubmatch(r.URL.Path)
-    if m == nil {
-        http.NotFound(w, r)
-        return "", errors.New("Invalid Page Title")
-    }
-    return m[2], nil // The title is the second subexpression.
+		body, err := ioutil.ReadAll(r.Body)
+		s := string(body)
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
+		}
+		// fmt.Println(u.Id)
+		fmt.Println(s)
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
