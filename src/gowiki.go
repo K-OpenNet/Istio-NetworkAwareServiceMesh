0b0b815f9a17cef73c6c81594af50dc5a7e3c82b
Chromatics <cr2025x1@gmail.com>
2018-03-06 01:19:16 +0900

    Partial decoding of JSON request

--- src/gowiki.go
+++ src/gowiki.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,27 +10,33 @@ import (
 
 var err error
 
-type User struct {
-	Id      string
-	Balance uint64
+type JSONCmd struct {
+	CmdType    string
+	CmdForm    string
+	CmdContent json.RawMessage
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// var u User
 		if r.Body == nil {
 			http.Error(w, "Please send a request body", 400)
 			return
 		}
 
-		body, err := ioutil.ReadAll(r.Body)
-		s := string(body)
+		reqBody, _ := ioutil.ReadAll(r.Body)
+		reqStr := string(reqBody)
+		var cmd JSONCmd
+		err = json.Unmarshal([]byte(reqStr), &cmd)
+
 		if err != nil {
 			http.Error(w, err.Error(), 400)
+			fmt.Println("Error occured")
 			return
 		}
-		// fmt.Println(u.Id)
-		fmt.Println(s)
+		fmt.Println("ReqBody: " + reqStr)
+		fmt.Println("CmdType: " + cmd.CmdType)
+		fmt.Println("CmdContent: " + string(cmd.CmdContent))
+		fmt.Println("CmdForm: " + cmd.CmdForm)
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
